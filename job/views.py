136ab85956b4232b183b07edca22cead57d9633c
Mohammed Hassan <mohammedhassan12_5@yahoo.com>
2023-10-24 21:27:59 +0300

    add detail

--- job/views.py
+++ job/views.py
@@ -6,3 +6,6 @@ def job_list(request):
     all_jobs = Job.objects.all()
     return render(request, 'job/job_list.html', {'jobs':all_jobs})
 
+def job_detail(request, slug):
+    job = Job.objects.get(slug=slug)
+    return render(request, 'job/job_detail.html', {'job':job})
