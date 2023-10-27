4b485894f8a4d863cad5256c42bf4559a9252568
Mohammed Hassan <mohammedhassan12_5@yahoo.com>
2023-10-27 02:22:30 +0200

    add list & detail jobs API using cbv

--- job/api.py
+++ job/api.py
@@ -1,9 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import JobSerializers
 from .models import Job
 
-
+'''
 @api_view(['GET'])
 def job_list_api(request):
     jobs = Job.objects.all()
@@ -15,6 +16,15 @@ def job_detail_api(request,id):
     job = Job.objects.get(id=id)
     data = JobSerializers(job).data
     return Response({'job':data})
+'''
 
-
+class JobListAPI(generics.ListAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializers
+    
+class JobDetailAPI(generics.RetrieveAPIView):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializers
+    
+    
     
--- job/urls.py
+++ job/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from .views import job_list, job_detail
-from .api import job_list_api, job_detail_api
+from .api import JobListAPI, JobDetailAPI #job_list_api, job_detail_api
 urlpatterns = [
     path('', job_list),
     path('<slug:slug>', job_detail),
     
     
-    path('api/list', job_list_api),
-    path('api/list/<int:id>', job_detail_api),
+    path('api/list/', JobListAPI.as_view()),
+    path('api/list/<int:pk>', JobDetailAPI.as_view()),
 ]
